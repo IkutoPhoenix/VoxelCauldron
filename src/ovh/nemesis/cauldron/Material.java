60cb6e4f76f76572f7401cbd7e7ac99499b15afa
Prince Ikuto Phoenix <33163837+IkutoPhoenix@users.noreply.github.com>
2020-10-27 17:15:20 +0100

    added friendly name to properties in Material

--- src/ovh/nemesis/cauldron/Material.java
+++ src/ovh/nemesis/cauldron/Material.java
@@ -3,7 +3,7 @@ package ovh.nemesis.cauldron;
 public class Material {
 
     private MaterialType materialType = MaterialType.DIFFUSE;
-    private MaterialProperty weight = new MaterialProperty("_weight", 1f), roughness = new MaterialProperty("_rough", 0.1f), specular_metal = new MaterialProperty("_spec", 0.5f), specular_plastic = new MaterialProperty("_spec_p", 0.5f), refraction = new MaterialProperty("_ior", 0.3f), attenuation = new MaterialProperty("_att", 0f), flux = new MaterialProperty("_flux", 0f), mix = new MaterialProperty("_gw", 0.7f), phase_0 = new MaterialProperty("_g0", -0.5f), phase_1 = new MaterialProperty("_g1", 0.8f), ldr = new MaterialProperty("_ldr", 0f);
+    private MaterialProperty weight = new MaterialProperty("_weight", "weigth", 1f), roughness = new MaterialProperty("_rough", "roughness", 0.1f), specular_metal = new MaterialProperty("_spec", "specular", 0.5f), specular_plastic = new MaterialProperty("_spec_p", "specular_plastic", 0.5f), refraction = new MaterialProperty("_ior", "refraction", 0.3f), attenuation = new MaterialProperty("_att", "attenuation", 0f), power = new MaterialProperty("_flux", "power", 0f), mix = new MaterialProperty("_gw", "mix", 0.7f), phase_0 = new MaterialProperty("_g0", "phase_0", -0.5f), phase_1 = new MaterialProperty("_g1", "phase_1", 0.8f), ldr = new MaterialProperty("_ldr", "ldr", 0f);
     /*
      *  diffuse : nothing
      *  metal : weight, specular_metal (_spec), roughness (_rough)
@@ -12,7 +12,7 @@ public class Material {
      *  cloud : weight, mix (_gw), phase_0 (_g0) [-0.90;0.90], phase_1 (_g1) [-0.90;0.90]
      *  emission : weight, power (_flux) [0;4], ldr
      */
-    private MaterialProperty[] properties = {weight, roughness, specular_metal, specular_plastic, refraction, attenuation, flux, mix, phase_0, phase_1, ldr};
+    private MaterialProperty[] properties = {weight, roughness, specular_metal, specular_plastic, refraction, attenuation, power, mix, phase_0, phase_1, ldr};
 
     public Material (MaterialType materialType) {
         this.materialType = materialType;
